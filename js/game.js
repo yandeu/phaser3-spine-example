44a341c76382e69354a6c84e7632bcb6558c50f7
yandeu <20306025+yandeu@users.noreply.github.com>
2019-12-11 13:29:29 +0100

    Updated to es2015 modules

---
+++ js/game.js
@@ -0,0 +1,28 @@
+﻿import MainScene from './mainScene.js'
+
+const config = {
+  type: Phaser.WEBGL,
+  backgroundColor: '#cdcdcd',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.DOM.CENTER_BOTH,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example'
+  },
+  plugins: {
+    scene: [
+      {
+        key: 'SpineWebGLPlugin',
+        plugin: SpineWebGLPlugin,
+        start: true,
+        sceneKey: 'spine'
+      }
+    ]
+  },
+  scene: [MainScene]
+}
+
+window.addEventListener('load', () => {
+  new Phaser.Game(config)
+})
